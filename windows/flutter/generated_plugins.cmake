1c7b525e1b636e5c9bb67405ebb8edd5ddb460ed
QuikAppTech <prasannasrie@gmail.com>
2025-06-13 13:06:25 +0530

    Publish Updated 01

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,16 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  app_links
+  connectivity_plus
+  firebase_core
+  flutter_inappwebview_windows
+  permission_handler_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
